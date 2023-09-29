bdfef6b9a76dec151380f6e062344d98f787f114
yahyacodes <yahya.codin@gmail.com>
2023-09-29 15:41:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json))
+    .catch((error) => console.error(error));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
+
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((json) => renderBooks(json))
+    .catch((error) => console.error(error));
+}
+
+function renderBooks(books) {
+  const bookListElement = document.getElementById("book-list");
+  if (!bookListElement) {
+    console.error("Book list element not found");
+    return;
+  }
+
+  bookListElement.innerHTML = ""; // Clear previous content
+
+  books.forEach((book) => {
+    const bookTitleElement = document.createElement("li");
+    bookTitleElement.textContent = book.title;
+    bookListElement.appendChild(bookTitleElement);
+  });
+}
+
+fetchBooks();
